a45592ece05dc95f9c596a503bc33414e7ded312
Kermo <kermo.ohno@voco.com>
2023-05-19 22:11:51 +0300

    API päring teise rakendusse: Elering päringu modifitseerimine ja API päring teise rakendusse: Makse

--- src/App.js
+++ src/App.js
@@ -1,57 +1,58 @@
+import { useRef } from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-    const [fi, setFi] = useState([]);
-    const [ee, setEe] = useState([]);
-    const [lv, setLv] = useState([]);
-    const [lt, setLt] = useState([]);
+    const [prices, setPrices] = useState([]);
+    const [chosenCountry, setChosenCountry] = useState("ee");
+    const [start, setStart] = useState("");
+    const [end, setEnd] = useState("");
+    const startRef = useRef();
+    const endRef = useRef();
 
     useEffect(() => {
-        fetch("http://localhost:4001/nord-pool-price")
-            .then(res => res.json())
-            .then(json => {
-                setFi(json.data.fi);
-                setEe(json.data.ee)
-                setLv(json.data.lv)
-                setLt(json.data.lt)
-            });
-    }, []);
+        if (start !== "" && end !== "") {
+            fetch("http://localhost:4001/nord-pool-price/" + chosenCountry + "/" + start + "/" + end)
+                .then(res => res.json())
+                .then(json => {
+                    setPrices(json);
+                });
+        }
+    }, [chosenCountry, start, end]);
+
+    function updateStart() {
+        const startIso = new Date(startRef.current.value).toISOString();
+        setStart(startIso);
+    }
+
+    function updateEnd() {
+        const endIso = new Date(endRef.current.value).toISOString();
+        setEnd(endIso);
+    }
 
     return (
         <div>
-            <table style={{marginLeft: "100px"}}>
-                <thead>
-                <th style={{border: "1px solid #ddd", padding: "12px", backgroundColor: "#04AA6D"}}>Ajatempel</th>
-                <th style={{border: "1px solid #ddd", padding: "12px", backgroundColor: "#04AA6D"}}>Hind</th>
-                </thead>
-                <tbody>
-                <div style={{position: "absolute", left: "30px"}}>Soome</div>
-                {fi.map(data =>
-                    <tr key={data.timestamp}>
-                        <td style={{border: "1px solid #ddd", padding: "8px"}}>{new Date(data.timestamp * 1000).toISOString()}</td>
-                        <td style={{border: "1px solid #ddd", padding: "8px"}}>{data.price}</td>
-                    </tr>)}
-                <div style={{position: "absolute", left: "30px"}}>Eesti</div>
-                {ee.map(data =>
-                    <tr key={data.timestamp}>
-                        <td style={{border: "1px solid #ddd", padding: "8px"}}>{new Date(data.timestamp * 1000).toISOString()}</td>
-                        <td style={{border: "1px solid #ddd", padding: "8px"}}>{data.price}</td>
-                    </tr>)}
-                <div style={{position: "absolute", left: "30px"}}>Läti</div>
-                {lv.map(data =>
-                    <tr key={data.timestamp}>
-                        <td style={{border: "1px solid #ddd", padding: "8px"}}>{new Date(data.timestamp * 1000).toISOString()}</td>
-                        <td style={{border: "1px solid #ddd", padding: "8px"}}>{data.price}</td>
-                    </tr>)}
-                <div style={{position: "absolute", left: "30px"}}>Leedu</div>
-                {lt.map(data =>
-                    <tr key={data.timestamp}>
-                        <td style={{border: "1px solid #ddd", padding: "8px"}}>{new Date(data.timestamp * 1000).toISOString()}</td>
-                        <td style={{border: "1px solid #ddd", padding: "8px"}}>{data.price}</td>
-                    </tr>)}
-                </tbody>
-            </table>
+            <button onClick={() => setChosenCountry("fi")}>Soome</button>
+            <button onClick={() => setChosenCountry("ee")}>Eesti</button>
+            <button onClick={() => setChosenCountry("lv")}>Läti</button>
+            <button onClick={() => setChosenCountry("lt")}>Leedu</button>
+            <input ref={startRef} onChange={updateStart} type="datetime-local" />
+            <input ref={endRef} onChange={updateEnd} type="datetime-local" />
+            {prices.length > 0 &&
+                <table style={{marginLeft: "100px"}}>
+                    <thead>
+                    <th style={{border: "1px solid #ddd", padding: "12px", backgroundColor: "#04AA6D"}}>Ajatempel</th>
+                    <th style={{border: "1px solid #ddd", padding: "12px", backgroundColor: "#04AA6D"}}>Hind</th>
+                    </thead>
+                    <tbody>
+                    <td style={{position: "absolute", left: "30px"}}>{chosenCountry}</td>
+                    {prices.map(data =>
+                        <tr key={data.timestamp}>
+                            <td style={{border: "1px solid #ddd", padding: "8px"}}>{new Date(data.timestamp * 1000).toISOString()}</td>
+                            <td style={{border: "1px solid #ddd", padding: "8px"}}>{data.price}</td>
+                        </tr>)}
+                    </tbody>
+                </table>}
         </div>
     );
 }
